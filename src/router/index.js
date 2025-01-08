be4d5253cd3e60e7530650c8752490c05883e907
Narek <narekkaravarichyan1910@gmail.com>
2025-01-09 01:01:54 +0400

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,8 @@
-// import { createRouter, createWebHistory } from 'vue-router';
-
-// // Импорт компонента HomeView
-// import HomeView from '@/views/HomeView.vue';
-
-// // Настройка маршрутов
-// const routes = [
-//   {
-//     path: '/',
-//     name: 'home',
-//     component: HomeView,
-//   },
-//   {
-//     path: '/posts',
-//     name: 'posts',
-//     component: () => import('@/components/PostTable.vue'), // Ленивый импорт таблицы постов
-//   },
-// ];
-
-// // Создание маршрутизатора
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes,
-// });
-
 import { createRouter, createWebHistory } from 'vue-router';
 
 import HomeView from '@/views/HomeView.vue';
 import PostDetail from '@/views/PostDetail.vue';
-import PostTable from '@/components/PostTable.vue'; 
+import PostTable from '@/components/PostTable.vue';
 
 const routes = [
   {
